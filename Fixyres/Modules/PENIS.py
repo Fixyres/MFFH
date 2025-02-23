7f567a0436bf3f2fcb78fe0906c38342ccf948b0
Artem <foxyy437@gmail.com>
2025-02-23 17:09:02 +0200

    New module: Fixyres/Modules/PENIS.py

---
+++ Fixyres/Modules/PENIS.py
@@ -0,0 +1,11 @@
+# meta developer: @xensidemod
+# requires: penis
+
+from .. import loader, utils
+import penis
+
+@loader.tds
+class PenisMod(loader.Module):
+    """Penis"""
+
+    strings = {"name": "Penis"}
