1554bb893a295ccdd516e969127007b34273b7d0
Artem <foxyy437@gmail.com>
2025-03-01 22:49:12 +0200

    Update: ZetGoHack/Nullmod/HornyHarem.py

--- ZetGoHack/Nullmod/HornyHarem.py
+++ ZetGoHack/Nullmod/HornyHarem.py
@@ -18,12 +18,13 @@
 
 
 # meta developer: @nullmod
-
+from telethon.tl.functions.channels import JoinChannelRequest, LeaveChannelRequest
+from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
+from hikkatl.tl.functions.messages import ImportChatInviteRequest
 from hikkatl.tl.types import Message
 from .. import loader, utils
 import asyncio
 import time
-
 
 @loader.tds
 class HornyHaremModule(loader.Module):
@@ -36,9 +37,9 @@ class HornyHaremModule(loader.Module):
         self.state = False
         self.outptt = True
         self.bonus = False
-        me = await self._client.get_me()
         self.id = 7896566560
         self.last_time = 0
+        self.lout = 0
 
     ########Ловец########
     @loader.watcher("only_messages","from_id=7896566560","only_media")
@@ -72,7 +73,7 @@ class HornyHaremModule(loader.Module):
     ########Заработок########
     @loader.command()
     async def autobonusW(self, message):
-        """Автоматически собирает бонус каждые 4 часа"""
+        """Автоматически собирает бонус(а также бонус за подписку и отыгрывает 3 игры в /lout) каждые 4 часа"""
         if self.bonus:
             self.bonus = False
             await message.edit("Автобонус выключен.")
@@ -80,14 +81,73 @@ class HornyHaremModule(loader.Module):
         self.bonus = True
         await message.edit("Автобонус включён.")
         while self.bonus:
-            await self.client.send_message(7896566560,"/bonus")
+            async with self._client.conversation(self.id) as conv:
+                await conv.send_message("/bonus")
+                r = await conv.get_response()
+                if "Доступен бонус за подписки" in r.text:
+                    await conv.send_message("/start flyer_bonus")
+                    r = await conv.get_response()
+                    if "проверка пройдена" not in r.text:
+                        to_leave = []
+                        to_block = []
+                        if r.reply_markup:
+                            a = r.buttons
+                            for i in a:
+                                for button in i:
+                                    if button.url:
+                                        if "t.me/+" in button.url:
+                                            try:
+                                                await self.client(ImportChatInviteRequest(button.url.split("+")[-1]))
+                                            except:
+                                                await asyncio.sleep(2)
+                                                await self.client(JoinChannelRequest(button.url))
+                                        url = button.url
+                                        if "?" in button.url:
+                                            url = button.url.split("?")[0]
+                                        entity = await self.client.get_entity(url)
+                                        if hasattr(entity,'broadcast'):
+                                            await self.client(JoinChannelRequest(button.url))
+                                            to_leave.append(entity.id)
+                                        elif hasattr(entity,'bot'):
+                                            try:
+                                                await self.client(UnblockRequest(entity.username))
+                                            except:
+                                                print('блин')
+                                            await self.client.send_message(entity,"/start")
+                                            to_block.append(entity.username)
+                            flyer_messages = await message.client.get_messages(self.id, limit=1)
+                            for m in flyer_messages:
+                                await asyncio.sleep(5)
+                                await m.click()
+                            for bot in to_block:
+                                await self.client(BlockRequest(bot))
+                                await self.client.delete_dialog(bot)
+                            for channel in to_leave:
+                                await self.client(LeaveChannelRequest(channel))
+                count = 0
+                if time.time()-self.lout > 86400:
+                    while count <= 3:
+                        await conv.send_message("/lout")
+                        r = await conv.get_response()
+                        if r.reply_markup:
+                            m = await r.respond(".")
+                            await self.lightsoutW(m,r)
+                            await m.delete()
+                            self.lout = time.time()
+                            count += 1
+                        else:
+                            break
+
+
+
             await asyncio.sleep(14400)
 
     @loader.command()
-    async def lightsoutW(self, message):
+    async def lightsoutW(self, message, r=None):
         """[ответ на соо с полем] Автоматически решает Lights Out"""
-        if message.is_reply:
-            r = await message.get_reply_message()
+        if message.is_reply or r:
+            if not r: 
+                r = await message.get_reply_message()
             if r.reply_markup:
                 a = r.buttons
                 pattern = []
